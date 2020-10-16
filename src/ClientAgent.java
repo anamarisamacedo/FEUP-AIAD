6977967789526568ca6bcd97ac9fcf06989a9b4e
omiguelgomes <miguelrgomes1698@gmail.com>
2020-10-16 12:26:59 +0100

    Added dummy agent to test classes

--- src/ClientAgent.java
+++ src/ClientAgent.java
@@ -2,7 +2,6 @@ import jade.core.Agent;
 
 public class ClientAgent extends Agent {
 	public void setup() {
-		Order newOrder = new Order(new int[] { 4, 5, 6 }, 25, "NewOrder");
-		System.out.println(newOrder.getWeight());
+
 	}
 }
