0db2ed0e48b0e6d2aff4895b46678c643151110d
Ana Macedo <anam.macedo98@gmail.com>
2020-11-04 09:26:00 +0000

    Added behavior for initiator request

--- src/SupplierAgent.java
+++ src/SupplierAgent.java
@@ -2,6 +2,7 @@ import jade.core.AID;
 import jade.core.Agent;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
+import jade.proto.AchieveREInitiator;
 import jade.proto.AchieveREResponder;
 
 import java.util.Vector;
@@ -10,8 +11,10 @@ public class SupplierAgent extends Agent {
 
 	public void setup() {
 		addBehaviour(new FIPARequestResp(this, MessageTemplate.MatchPerformative(ACLMessage.REQUEST)));
+		addBehaviour(new FIPARequestInit(this, new ACLMessage(ACLMessage.REQUEST)));
 	}
 	
+	//Cyclic behavior??
 	class FIPARequestResp extends AchieveREResponder {
 
 		public FIPARequestResp(Agent a, MessageTemplate mt) {
@@ -32,6 +35,38 @@ public class SupplierAgent extends Agent {
 			//result.setContentObject(); <-- any object (vectors, lists, etc...)
 			//result.setContent("Welelele");
 			return result;
+		}
+
+	}
+	
+	class FIPARequestInit extends AchieveREInitiator {
+
+		public FIPARequestInit(Agent a, ACLMessage msg) {
+			super(a, msg);
+		}
+
+		protected Vector<ACLMessage> prepareRequests(ACLMessage msg) {
+			Vector<ACLMessage> v = new Vector<ACLMessage>();
+			msg.addReceiver(new AID("DistributorAgent", false));
+			msg.setContent("Distribute this 10 orders!");
+			v.add(msg);
+			return v;
+		}
+		
+		protected void handleAgree(ACLMessage agree) {
+			System.out.println(agree);
+		}
+		
+		protected void handleRefuse(ACLMessage refuse) {
+			System.out.println(refuse);
+			}
+		
+		protected void handleInform(ACLMessage inform) {
+			System.out.println(inform);
+			}
+		
+		protected void handleFailure(ACLMessage failure) {
+			System.out.println(failure);
 		}
 
 	}
