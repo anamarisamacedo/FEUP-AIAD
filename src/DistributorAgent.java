0d788fe36f0ad584bf055ac1c7d01a40af0f8be9
omiguelgomes <miguelrgomes1698@gmail.com>
2020-11-12 13:16:25 +0000

    added distributor

---
+++ src/DistributorAgent.java
@@ -0,0 +1,55 @@
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
+import jade.proto.AchieveREResponder;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/*
+ * Agent for the Distribution
+ * */
+public class DistributorAgent extends Agent {
+
+    public void setup() {
+        addBehaviour(new FIPARequestResp(this, MessageTemplate.MatchPerformative(ACLMessage.REQUEST)));
+    }
+
+    //Receives requests from the supplier
+    class FIPARequestResp extends AchieveREResponder {
+
+        public FIPARequestResp(Agent a, MessageTemplate mt) {
+            super(a, mt);
+        }
+
+        protected ACLMessage handleRequest(ACLMessage request) {
+            try {
+                ArrayList<Order> orders = (ArrayList<Order>)(request.getContentObject());
+                Arrays.toString(orders.toArray());
+                System.out.println("Got the orders, here is the first's date: " + orders.get(0).getDate());
+                //send order array
+                //addBehaviour( new FIPARequestSupplierInit(this, new ACLMessage(ACLMessage.REQUEST), orders ));
+
+            } catch (UnreadableException e) {
+                e.printStackTrace();
+            }
+            ACLMessage reply = request.createReply();
+            reply.setPerformative(ACLMessage.AGREE);
+            return reply;
+        }
+
+        protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) {
+            ACLMessage result = request.createReply();
+            result.setPerformative(ACLMessage.INFORM);
+            result.setContent("Distr: Request received! We will start distributing the orders!");
+            return result;
+        }
+
+
+    }
+}
